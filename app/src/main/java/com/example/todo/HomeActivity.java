8d4bae03081aa7044b887ca6d2e0fadcc9206e48
ThuNhu2801 <hoangthithunhu28012001@gmail.com>
2022-06-04 16:12:36 +0700

    create alertDialog and insert data to firebase realtime db

--- app/src/main/java/com/example/todo/HomeActivity.java
+++ app/src/main/java/com/example/todo/HomeActivity.java
@@ -1,14 +1,144 @@
 package com.example.todo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+    private DatabaseReference reference;
+
+    private ProgressDialog loader;
+
+    private String key = "";
+    private String task;
+    private String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        recyclerView =findViewById((R.id.recyclerView));
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        loader=new ProgressDialog(this);
+
+        mUser=mAuth.getCurrentUser();
+        onlineUserID=mUser.getUid();
+        reference= FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserID);
+
+
+        floatingActionButton=findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
     }
+    private void addTask() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.input_file, null);
+        myDialog.setView(myView);
+
+        AlertDialog dialog =myDialog.create() ;
+        dialog.setCancelable(false);
+
+        final EditText task=myView.findViewById(R.id.task);
+        final EditText description=myView.findViewById(R.id.description);
+
+        Button save=myView.findViewById(R.id.saveBtn);
+        Button cancel=myView.findViewById(R.id.cancelBtn);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mTask = task.getText().toString().trim();
+                String mDescription = description.getText().toString().trim();
+                String id = reference.push().getKey();
+                String date = DateFormat.getDateInstance().format(new Date());
+
+
+                if (TextUtils.isEmpty(mTask)) {
+                    task.setError("Task Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(mDescription)) {
+                    description.setError("Description Required");
+                    return;
+                } else {
+                    loader.setMessage("Adding your data");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+//
+                    Model model = new Model(mTask, mDescription, id, date);
+                    reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(HomeActivity.this, "Task has been inserted successfully", Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }else{
+                                String error = task.getException().toString();
+                                Toast.makeText(HomeActivity.this, "Failed: " + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+
+                }
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
 }
